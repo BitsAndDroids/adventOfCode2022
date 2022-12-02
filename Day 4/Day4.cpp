d3f92d91fd0d7c656a09c1f602148d89db542e81
Dave Riedel <info@bitsanddroids.com>
2022-12-02 07:51:30 +0100

    folders for day3 + 4

---
+++ Day 4/Day4.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <chrono>
+//
+// Created by DaveRiedel on 1-12-2022.
+//
+int main(){
+    //start timer
+    auto start = std::chrono::high_resolution_clock::now();
+
+    //Read file into string
+    std::ifstream file("day3.txt");
+    std::string text;
+    while (std::getline(file, text)) {
+        if (text.empty()) {
+
+        } else {
+
+        }
+    }
+
+    //end timer
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+    //in ....
+    std::cout <<"Answer: "<< "in: " << elapsed.count() << std::endl;
+        return 0;
+}
