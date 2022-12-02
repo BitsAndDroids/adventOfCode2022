96bea4aa44dc5d822b27555229f19e3f2d80e89c
Dave Riedel <info@bitsanddroids.com>
2022-12-02 08:28:47 +0100

    Removed some garbage

--- Day2/Day2B.cpp
+++ Day2/Day2B.cpp
@@ -2,15 +2,17 @@
 #include <fstream>
 #include <string>
 #include <chrono>
+
 #include "RESULTENUM.h"
 #include "RPCENUM.h"
-#include "Result.h"
 //
 // Created by DaveRiedel on 1-12-2022.
 //
 #define FIRSTROCK "A"
 #define FIRSTPAPER "B"
-#define FIRSTSCISSORS "C"
+#define SECONDLOSS "X"
+#define SECONDDRAW "Y"
+
 //Alternative solution
 int move[8][3] = {
         {ROCK, PAPER, SCISSORS},
@@ -31,19 +33,15 @@ int main(){
     std::string text;
     int counter = 0;
     while (std::getline(file, text)) {
-
         if (text.empty()) {
-
         } else {
             std::string substring = text.substr(0, text.find(' '));
             std::string secondSubstring = text.substr(text.find(' ') + 1);
             int first = (substring == FIRSTROCK) ? ROCK : (substring == FIRSTPAPER) ? PAPER : SCISSORS;
-            RESULT second = (secondSubstring == "X") ? LOSS : (secondSubstring == "Y") ? DRAW : WIN;
-            //Another compacter way to do this
+            RESULT second = (secondSubstring == SECONDLOSS) ? LOSS : (secondSubstring == SECONDDRAW) ? DRAW : WIN;
             counter += second;
             //The first row is the first player's move, the second row is the second player's move.
             counter += move[second + 1][first - 1];
-
         }
     }
     //end timer
